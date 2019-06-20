5673d00f2ad473eb646de0a3479d5e2b4df95c7d
Varga János <vargajana95@gmail.com>
2019-06-20 12:47:16 +0200

    Added messages list

---
+++ src/main/java/com/progmatic/messenger2019spring/controller/MessageController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.progmatic.messenger2019spring.controller;
+
+import com.progmatic.messenger2019spring.model.Message;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Varga János
+ */
+@Controller
+public class MessageController {
+    
+    private List<Message> messages;
+
+    public MessageController() {
+        this.messages = new ArrayList<>();
+        
+        messages.add(new Message("Hello there", "Obi wan", LocalDateTime.of(2019, Month.MARCH, 12, 17, 13, 0)));
+        messages.add(new Message("Programozni jó", "Progmatic"));
+        messages.add(new Message("Ez egy teszt üzenet", "Test"));
+        messages.add(new Message("Meg ez is", "Test"));
+        messages.add(new Message("Meg még ez is", "Test"));
+    }
+    
+    @RequestMapping(value = "/messages", method = RequestMethod.GET)
+    public String listMessages(Model model) {
+        model.addAttribute("messages", messages);
+        
+        return "messages";
+    }
+}
---
+++ src/main/java/com/progmatic/messenger2019spring/model/Message.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.progmatic.messenger2019spring.model;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Varga János
+ */
+public class Message {
+    private String text;
+    private String author;
+    
+    private LocalDateTime dateTime;
+
+    public Message(String text, String author) {
+        this.text = text;
+        this.author = author;
+        this.dateTime = LocalDateTime.now();
+    }
+
+    
+    
+    public Message(String text, String author, LocalDateTime dateTime) {
+        this.text = text;
+        this.author = author;
+        this.dateTime = dateTime;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+    
+    
+    
+    
+}
